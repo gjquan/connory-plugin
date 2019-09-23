8951c99f01a11296841963c8bfa26b4e9f923e3d
Greg Quan <gquan@adap.tv>
2019-09-22 17:05:58 -0700

    add package.json and rearrange files

---
+++ src/feed.js
@@ -0,0 +1,138 @@
+'use strict';
+
+const FEED_URL = 'https://www.instagram.com/explore/tags/connoryeverafter/';
+const STATIC_POST_DISPLAY_TIME = 5000;
+
+function addLoadCallback(callback) {
+  if (document.readyState === 'complete') {
+    callback();
+  } else {
+    window.addEventListener('load', callback);
+  }
+}
+
+function start() {
+	_appendHashtagBanner();
+	_startFeed();
+}
+
+function _appendHashtagBanner() {
+	// TODO
+}
+
+function _startFeed() {
+	const PAGE_POST_SELECTOR = '.v1Nh3.kIKUG._bz0w a';
+
+	let post = document.querySelector(PAGE_POST_SELECTOR);
+	post.click();
+	_playCurrentPost().then(function() {
+		_advanceFeed();
+	});
+}
+
+function _advanceFeed() {
+	const POST_NEXT_SELECTOR = 'button._6CZji';
+	const PAGINATION_NEXT_SELECTOR = '.coreSpriteRightPaginationArrow';
+
+	let nextPost;
+	let nextImage;
+	let mediaPlay;
+
+	nextImage = document.querySelector(POST_NEXT_SELECTOR);
+	if (nextImage) {
+		nextImage.click();
+		_playCurrentPost().then(function() {
+			setTimeout(_advanceFeed, 0);
+		});
+	} else {
+		nextPost = document.querySelector(PAGINATION_NEXT_SELECTOR);
+		if (nextPost) {
+			nextPost.click();
+			_playCurrentPost().then(function() {
+				setTimeout(_advanceFeed, 0);
+			});
+		} else {
+			// no next post; back to the beginning
+			// disabled during testing
+			// window.location.href = FEED_URL;
+			return;
+		}
+	}
+}
+
+function _playCurrentPost() {
+	let currentPost = _currentPost();
+	let returnPromise = new Promise(function(resolve, reject) {
+		if (_postHasStoppedVideo(currentPost)) {
+			let currentVideoElement = currentPost.querySelector('video');
+			if (currentVideoElement) {
+				let playedOnce = false; // keep track of whether the video has looped at least once
+				currentVideoElement.muted = true; // can only autplay muted video
+				currentVideoElement.addEventListener('timeupdate', function() {
+					if (currentVideoElement.currentTime > currentVideoElement.duration / 2) {
+						playedOnce = true;
+					} else if (playedOnce) {
+						resolve(true);
+					}
+				});
+				_playPostVideo(currentPost);
+			} else {
+				setTimeout(function() { resolve(null); }, STATIC_POST_DISPLAY_TIME);
+			}
+		} else {
+			setTimeout(function() { resolve(null); }, STATIC_POST_DISPLAY_TIME);
+		}
+	});
+
+	return returnPromise;
+}
+
+function _currentPost() {
+	const POPUP_SELECTOR = '.M9sTE';
+	const POPUP_POST_SELECTOR = POPUP_SELECTOR + ' .OAXCp';
+	const CAROUSEL_CONTAINER_SELECTOR = POPUP_SELECTOR + ' .MreMs';
+
+	let posts = document.querySelectorAll(POPUP_POST_SELECTOR);
+	if (!posts) {
+		return;
+	}
+
+	if (posts.length === 1) {
+		return posts[0];
+	}
+
+	let carouselContainer = document.querySelector(CAROUSEL_CONTAINER_SELECTOR);
+	if (carouselContainer) {
+		let carouselContainerStyle = carouselContainer.style ? carouselContainer.style.transform : '';
+		let carouselContainerWidth = carouselContainer.getBoundingClientRect().width;
+		let match = /translateX\(([-0-9]+)px/.exec(carouselContainerStyle);
+		if (match) {
+			let translateX = parseInt(match[1], 10);
+			if (translateX < 0) { translateX *= -1; }
+
+			let currentPostIndex = translateX / carouselContainerWidth;
+			if (posts.length > currentPostIndex) {
+				return posts[currentPostIndex];
+			}
+		}
+	}
+}
+
+function _postHasStoppedVideo(post) {
+	const VIDEO_PLAY_GLYPH_VISIBLE_SELECTOR = '.videoSpritePlayButton.PTIMp';
+
+	return post ? post.querySelector(VIDEO_PLAY_GLYPH_VISIBLE_SELECTOR) : false;
+}
+
+function _playPostVideo(post) {
+	const VIDEO_PLAY_SELECTOR = 'a.QvAa1';
+
+	if (_postHasStoppedVideo(post)) {
+		let playButton = post.querySelector(VIDEO_PLAY_SELECTOR);
+		if (playButton) {
+			playButton.click();
+		}
+	}
+}
+
+addLoadCallback(start);
